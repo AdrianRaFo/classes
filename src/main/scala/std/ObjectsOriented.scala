5f60e3af47588105129028a2c1410c0dd9b21d7f
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2020-10-20 21:11:40 +0200

    Add content

--- src/main/scala/std/ObjectsOriented.scala
+++ src/main/scala/std/ObjectsOriented.scala
@@ -1,5 +1,6 @@
 package std
 
+//unique instance
 object ObjectsOriented {
 
   def main(args: Array[String]): Unit = {
@@ -7,3 +8,24 @@ object ObjectsOriented {
   }
 
 }
+
+//multiple instances
+class MyClass(name: String, age: Int) {
+
+
+  val noMutable = true
+
+  var mutable = false
+  mutable = true
+
+  def executedWhenCalled() = true //methods, the last thing written is the one returned
+
+  lazy val winy: String = throw new RuntimeException("Winy is lazy even to fail") // Executed the first time is called
+  println("ha ha") //This is printed
+  println(winy) //Exception thrown here because it's executed here
+
+}
+
+//used to model
+case class MyCaseClass(name: String, age: Int)
+
--- src/main/scala/std/Types.scala
+++ src/main/scala/std/Types.scala
@@ -2,4 +2,22 @@ package std
 
 object Types {
 
+  val text: String = "abcds"
+  val number: Int = 123
+  val decimal: Double = 123.23
+
+  val list: List[Int] = List(1, 2, 3)
+  val listWithoutDuplicates = Set(1, 1, 2, 3) //Set(1, 2, 3)
+  val map: Map[Int, String] = Map(1 -> "a", 2 -> "b")
+
+  val tuples: (Int, String, Boolean) = (1, "a", true)
+
+  val optional: Option[Int] = Some(1)
+  val empty: Option[Int] = None
+
+  val rightOrLeft:Either[String, Int] = Right(1) //this can be Int or String but it's Int
+  val leftOrRight: Either[String, Int] = Left("left path") //this can be Int or String but it's String
+
+  
+
 }
