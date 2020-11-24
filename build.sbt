47ad781bcf05f6a4be53b72922d2856f4832fbfd
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2020-11-24 20:19:15 +0100

    generator tests

--- build.sbt
+++ build.sbt
@@ -5,8 +5,8 @@ version := "0.1"
 scalaVersion := "2.13.3"
 
 libraryDependencies ++= Seq(
-  "org.scalatest"  %% "scalatest-funsuite"          % "3.2.3"  % Test,
-  "org.scalatest"  %% "scalatest-shouldmatchers"    % "3.2.3"  % Test,
-  "org.scalacheck" %% "scalacheck"                  % "1.14.1" % Test,
-  "com.47deg"      %% "scalacheck-toolbox-datetime" % "0.4.0"  % Test
+  "org.scalatest"     %% "scalatest-funsuite"       % "3.2.3"   % Test,
+  "org.scalatest"     %% "scalatest-shouldmatchers" % "3.2.3"   % Test,
+  "org.scalatestplus" %% "scalacheck-1-14"          % "3.2.2.0" % Test,
+  "org.scalacheck"    %% "scalacheck"               % "1.14.1"  % Test
 )
--- src/test/scala/std/exercises/recursivity/RecursiveTest.scala
+++ src/test/scala/std/exercises/recursivity/RecursiveTest.scala
@@ -1,32 +1,52 @@
 package std.exercises.recursivity
 
-import org.scalatest.funsuite.AnyFunSuite
 import java.time.LocalDate
 
+import org.scalacheck.Gen
+import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 import std.exercises.tagless.ConsoleIO
 
 import scala.util.Try
 
-class RecursiveTest extends AnyFunSuite with Matchers {
+class RecursiveTest extends AnyFunSuite with Matchers with ScalaCheckDrivenPropertyChecks {
 
   val today: LocalDate = LocalDate.now()
+
+  val localDateGen: Gen[LocalDate] =
+    Gen.choose(today.toEpochDay, today.withDayOfYear(365).toEpochDay).map(LocalDate.ofEpochDay)
 
   test("Intento de testear lo que habeis hecho") {
 
     val fakeConsoleIO: ConsoleIO[Try] = new ConsoleIO[Try] {
       def readBoolean: Try[Boolean] = throw new RuntimeException("This should not be used")
-      def readInt: Try[Integer] = throw new RuntimeException("This should not be used")
-      def readString: Try[String] = Try("10/12/2020")
+      def readInt: Try[Integer]     = throw new RuntimeException("This should not be used")
+      def readString: Try[String]   = Try("10/12/2020")
     }
 
     val ciberpunk = new CyberpunkImp(fakeConsoleIO)
 
     //Al incluir la fecha en el mensaje tb testeamos que se lanza en la fecha que esperamos
-    //La fecha deberia ser inicial.plusDays(80)
     val expectedFinalMessage = "28/02/2021 - YA HA SALIDO CYBERPUNK, POR FIN. SE ME SALTAN LAS LAGRIMAS!!!"
     ciberpunk.passingDays(today) shouldBe expectedFinalMessage
     //Al parecer al devolver el String final esto **SI** se puede testear
+  }
+
+  test("Test con generadores") {
+    forAll(localDateGen) { localDate =>
+      val fakeConsoleIO: ConsoleIO[Try] = new ConsoleIO[Try] {
+        def readBoolean: Try[Boolean] = throw new RuntimeException("This should not be used")
+        def readInt: Try[Integer] = throw new RuntimeException("This should not be used")
+        def readString: Try[String] = Try(CyberpunkImp.pattern.format(localDate))
+      }
+
+      val ciberpunk = new CyberpunkImp(fakeConsoleIO)
+      val expectedFinalMessage =
+        s"${CyberpunkImp.pattern.format(localDate.plusDays(80))} - YA HA SALIDO CYBERPUNK, POR FIN. SE ME SALTAN LAS LAGRIMAS!!!"
+
+      ciberpunk.passingDays(today) shouldBe expectedFinalMessage
+    }
   }
 
 }
