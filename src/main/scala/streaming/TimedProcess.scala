0a77f381e98be9360db5184a8a75ef3f316015c3
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2020-12-18 20:17:09 +0100

    improve naming

--- src/main/scala/streaming/TimedProcess.scala
+++ src/main/scala/streaming/TimedProcess.scala
@@ -7,12 +7,12 @@ import fs2.Stream
 
 object TimedProcess extends IOApp {
 
-  def doSomething(num: Long) = IO(println(s"something $num"))
+  def process(seconds: Long): IO[Unit] = IO(println(s"Emitted after $seconds seconds from the beginning"))
 
-  def usingStreams[A](doIO: Long => IO[A]) =
+  def infiniteStreaming[A](doIO: Long => IO[A]): Stream[IO, A] =
     Stream.awakeEvery[IO](2.seconds).evalMap(dur => doIO(dur.toSeconds)).repeat
 
   override def run(args: List[String]): IO[ExitCode] =
-    usingStreams(doSomething).compile.drain.as(ExitCode.Success)
+    infiniteStreaming(process).compile.drain.as(ExitCode.Success)
 
 }
